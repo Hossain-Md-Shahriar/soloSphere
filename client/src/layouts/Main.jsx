bb0fc0b49d868492c94f722d957d7c66b91a1e7f
Hossain-Md-Shahriar <hasibshahriar1@gmail.com>
2024-05-15 22:20:37 +0600

    Authentication done banner added

--- client/src/layouts/Main.jsx
+++ client/src/layouts/Main.jsx
@@ -1,11 +1,17 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Main = () => {
   return (
     <div>
+      <Navbar />
+      <div className="min-h-[calc(100vh-306px)]">
       <Outlet />
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
